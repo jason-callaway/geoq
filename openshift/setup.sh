29b15d1f82830c7ef85e1a3fab5b0db762eb07b5
Jason Callaway <jason@jasoncallaway.com>
2016-04-23 22:55:10 -0400

    modifying setup.sh

--- openshift/setup.sh
+++ openshift/setup.sh
@@ -7,7 +7,7 @@ source ~/geoq/bin/activate
 #git clone https://github.com/ngageoint/geoq.git
 #pushd geoq
 
-cat >queries.sql << EOF
+cat > ~/queries.sql << EOF
 create role geoq login password 'geoq';
 create database geoq with owner geoq;
 \c geoq
@@ -15,7 +15,7 @@ create extension postgis;
 create extension postgis_topology;
 EOF
 # This sucks, need to make these queries idempotent. For now always return true.
-PGPASSWORD=${DATABASE_PASSWORD} psql -h pg-master -U postgres -f ./queries.sql || true
+PGPASSWORD=${DATABASE_PASSWORD} psql -h pg-master -U postgres -f ~/queries.sql || true
 
 #export PATH=$PATH:/usr/pgsql-9.4/bin
 pip install paver
@@ -26,11 +26,11 @@ paver install_dev_fixtures
 npm install -g less
 
 #cat << EOF > geoq/local_settings.py
-cat << EOF > local_settings.py
+cat << EOF > ~/geoq/local_settings.py
 STATIC_URL_FOLDER = '/static'
 STATIC_ROOT = '{0}{1}'.format('/var/www/html', STATIC_URL_FOLDER)
 EOF
 
-popd
-python manage.py collectstatic
-python manage.py createsuperuser
+#popd
+python ~/manage.py collectstatic
+python ~/manage.py createsuperuser
