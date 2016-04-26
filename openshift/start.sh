6e6d57404a75d5c42898857cc4ce28951a1a6a91
Jason Callaway <jason@jasoncallaway.com>
2016-04-26 17:10:03 -0500

    working on listen_address

--- openshift/start.sh
+++ openshift/start.sh
@@ -4,7 +4,7 @@ pushd /opt/app-root/src
 ./openshift/setup.sh
 source ./geoq_virtualenv/bin/activate
 LISTEN_ADDRESS=$(ip a | grep eth0 | grep inet | awk '{print $2}' | cut -f1 -d'/')
-paver start_django -b ${LISTEN_ADDRESS}
+paver start_django -b ${LISTEN_ADDRESS}:8000
 while true; do
     sleep 60
 done
